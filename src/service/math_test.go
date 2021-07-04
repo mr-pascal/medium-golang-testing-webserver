5f0d95eca3f7055cb32fc2f5d2457f91c372da86
Abszissex <rubSXkwZDZGqrr4>
2021-07-04 15:48:03 +0200

    added comments and additional test cases for `math_test.go`

--- src/service/math_test.go
+++ src/service/math_test.go
@@ -13,22 +13,31 @@ func TestSum(t *testing.T) {
 }
 
 func TestMultiply(t *testing.T) {
-	// Using test tables
+
+	// Create the test table
 	tables := []struct {
 		x int
 		y int
 		r int
 	}{
-		{x: 1, y: 1, r: 1},
-		{1, 2, 2},
-		{0, 0, 0},
-		{2, 0, 0},
-		{-5, 2, -10},
+		{x: 1, y: 2, r: 2}, // normal multiplication with named parameters
+		{2, 3, 6},          // normal multiplication
+		{0, 0, 0},          // both zero
+		{2, 0, 0},          // x is zero
+		{0, 2, 0},          // y is zero
+		{-5, 2, -10},       // x is negative
+		{5, -2, -10},       // y is negative
+		{-5, -2, 10},       // x and y are negative
 	}
 
 	for _, table := range tables {
+		// Get the return value of the function
 		total := Multiply(table.x, table.y)
+
+		// Assert the result with our expected value
 		if total != table.r {
+			// Doesn't match our expected result
+			// -> Print error indicating what was the input, what we got and what we have expected
 			t.Errorf("Multiplication of ( %d * %d ) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.r)
 		}
 	}
